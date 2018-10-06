cf6fba4e7cacd3dd64008ab5c9a9571bfdc30732
Jacob Mortensen <jacob@jacobmortensen.com>
2018-10-06 16:19:27 -0700

    Get events from infura

--- Frontend/client/src/start-audit/StartAudit.js
+++ Frontend/client/src/start-audit/StartAudit.js
@@ -1,34 +1,39 @@
 import React, { Component } from "react";
 import getWeb3 from "../utils/getWeb3";
 import BigNumber from 'bignumber.js';
+import Web3 from "web3";
 
 import "./StartAudit.css";
-import QuanstampAbi from "./Quanstamp.abi.js";
+import QuanstampTokenAbi from "./QuanstampToken.abi";
+import QuantstampAuditAbi from "./QuantstampAudit.abi";
+
 const QuantstampTokenAddress = '0x99ea4db9ee77acd40b119bd1dc4e33e1c070b80d';
 const QuantstampContractAddress = '0x74814602062af64fd7a83155645ddb265598220e';
 
 class StartAudit extends Component {
-  state = { balance: 0, authorized: 0, web3: null, accounts: null, authorizeContract: null };
+  state = { balance: 0, authorized: 0, web3: null, accounts: null, authorizeContract: null, auditContract: null };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
+      const web3Socket = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'));
       const web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-      let authorizeContract = new web3.eth.Contract(QuanstampAbi, QuantstampTokenAddress);
+      const authorizeContract = new web3.eth.Contract(QuanstampTokenAbi, QuantstampTokenAddress);
+      const auditContract = new web3.eth.Contract(QuantstampAuditAbi, QuantstampContractAddress);
+      const auditContractEvents = new web3Socket.eth.Contract(QuantstampAuditAbi, QuantstampContractAddress);
 
-
-      this.setState({ web3: web3, account: accounts[0], authorizeContract: authorizeContract });
+      this.setState({ web3: web3, account: accounts[0], authorizeContract: authorizeContract, auditContract: auditContract });
       const qspBalance = await this.getQpsBalance();
       const amountLeftover = await this.getAmountOfAuthorized();
 
       console.log(qspBalance);
       console.log(amountLeftover);
       this.setState({ balance: qspBalance, authorized: amountLeftover });
-      // this.authorizeQuantstamp();
 
+      auditContractEvents.events.LogAuditRequested({ fromBlock: 0 }, this.auditEventFired);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -47,16 +52,20 @@ class StartAudit extends Component {
   }
 
   authorizeQuantstamp = async () => {
-    this.state.authorizeContract.methods.approve(QuantstampContractAddress, 3000).send({ from: this.state.account });
+    await this.state.authorizeContract.methods.approve(QuantstampContractAddress, 3000).send({ from: this.state.account });
   };
 
-  requestAudit = async () => {
-
+  requestAudit = async (uri, price) => {
+    await this.state.auditContract.methods.requestAudit(uri, price).send({ from: this.state.account });
   }
 
   humanReadable(erc20Balance) {
     const divisor = new BigNumber(10).pow(18); // Quanstamp ERC20 is 18 decimals
     return new BigNumber(erc20Balance).div(divisor).toNumber();
+  }
+
+  auditEventFired(err, event) {
+    console.log(event);
   }
 
 
